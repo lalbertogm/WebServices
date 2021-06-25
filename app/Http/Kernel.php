52d7b20b8d45b289bd92b8f9ca4d43d9d173b6e1
Alexander Franco <alexandeer2600@gmail.com>
2021-06-25 11:56:37 -0500

    Integracion  middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,5 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\EnsureTokenIsValid::class,               //---- Se incluyo la ruta del middleware
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('obtener/generos', [ApiController::class,'obtenerGeneros'])->name("obtener.generos");
-Route::get('obtener/genero/{name}', [ApiController::class,'obtenerGenero'])->name("obtener.genero");
-Route::get('obtener/artista/{id}', [ApiController::class,'obtenerArtista'])->name("obtener.artista");
+Route::get('obtener/genero/{name}',  [ApiController::class,'obtenerGenero'])->name("obtener.genero");
+
+Route::group(['middleware' => 'admin'], function () {  //----Grupo de rutas que pasan primero por el middleware
+
+    Route::get('obtener/artista/{id}',[ApiController::class,'obtenerArtista'])->name("obtener.artista");
+
+});
